d114f09004353bd32b2ace207d20f9e0f1868f21
cwmli <calvinl9331@gmail.com>
2019-01-02 13:33:23 -0500

    Displaying playlist tracks

--- client/src/components/playlist/playlist.js
+++ client/src/components/playlist/playlist.js
@@ -20,9 +20,42 @@ class ConnectedPlaylist extends Component {
   }
   
   render() {
-    return (
-      <div>{this.props.match.params.id}</div>
-    )
+    if (this.props.queriedPlaylist && !this.props.queriedPlaylist.isFetching) {
+      let playlist = this.props.queriedPlaylist.data;
+
+      return (
+        <div className="pa4">
+          <div className="w-30-ns w-40-m flex flex-column pr3 br b--black-10">
+            <div className="flex items-center pb1">
+              <img className="w3 h3 pr3" src={playlist.images[0].url} alt='playlist-image' />
+              <dl className="mt2 mb1 lh-copy">
+                <dd className="ml0 f4 black b truncate w-100">{playlist.name}</dd>
+                <dd className="ml0 f6 gray truncate w-100">{playlist.followers.total} Followers - {playlist.tracks.total} Tracks</dd>
+                <dd className="ml0 f6 gray truncate w-100">by {playlist.owner.display_name}</dd>
+              </dl>
+            </div>
+            <div className="f6 gray bb b--black-10 bw1 pb3" dangerouslySetInnerHTML={{__html: playlist.description}} />
+            <div className="flex flex-column flex-wrap">
+            {playlist.tracks.items.map((trackObj, i) => {
+              return (
+                <a key={i} href={trackObj.track.external_urls.spotify} className="flex dim items-center link lh-copy pa1 ph0-l bb b--black-10">
+                  <img className="w2 h2 br3" src={trackObj.track.album.images[0].url} alt={trackObj.track.album.name + '-avatar'} />
+                  <div className="pl3 flex-auto">
+                    <span className="f6 b db black">{trackObj.track.name}</span>
+                    <span className="f6 db black">{trackObj.track.artists[0].name} - {trackObj.track.album.name}</span>
+                  </div>
+                </a>
+              )
+            })}
+            </div>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div>LOADING</div>
+      )
+    }
   }
 }
 
